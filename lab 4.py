05bef05f8a77705338fcdd051d148c42bf86b84e
1 <abc@gmail.com>
2023-10-04 15:29:37 +0300

    lab 4

--- lab 4.py
+++ lab 4.py
@@ -1,5 +1,5 @@
 #  Попов Юрий Александрович ИУ7-12Б
-# Таблица значений и построение графика
+# Таблица значений и построение графика в осях z y
 import math
 
 '''Блок ввода'''
@@ -9,11 +9,13 @@ start_z, end_z, step = map(float, input('Введите через пробел 
 p1 = lambda z: (z ** 4) - (3 * z ** 3) + (8 * z ** 2) - 5
 p2 = lambda z: (10.125 * z ** 0.5) - 20.15 * math.cos((math.pi / 2) * z)
 
+draw_p_axis = False
+
 
 #  Делаем таблицу
-
 def make_table() -> list:
-    '''Функция строит таблицу значений для функции и возвращает массив, состоящий из кортежа типа (z, p1, p2)'''
+    global draw_p_axis
+    '''Функция строит таблицу значений для функции и возвращает массив, состоящий из кортежа типа (z, p1)'''
     table = []
     print(f'\n{"Таблица значений для 2 функций":^106}')
     print('—' * 105)
@@ -25,12 +27,19 @@ def make_table() -> list:
         cur_z1 = p1(cur_z)  # Получаем значение функции p1 and p2
         cur_z2 = p2(cur_z)
 
+        if cur_z1 < 0:
+            draw_p_axis = True
         if round(cur_z, 2) <= end_z:
             print(f'|{i + 1:^25.6g}|{cur_z:^25.6g}|{cur_z1:^25.6g}|{cur_z2:^25.6g}|')
-            table.append((cur_z, cur_z1, cur_z2))
+            table.append((cur_z, cur_z1))
 
     print('—' * 105)
     return table
 
 
 table = make_table()
+size = 120  # размер графика
+serifs = int(input('Введите количество засечек на оси Y (По умолчанию 8): ') or 8)
+
+if not (4 <= serifs <= 8): # проверка на колво засечек
+    pass
