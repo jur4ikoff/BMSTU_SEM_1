84f3a0a0d44d63a60bf5e3d738df360cd0c7c147
1 <abc@gmail.com>
2023-10-16 11:41:03 +0300

    lab 6

--- lab 6.py
+++ lab 6.py
@@ -2,8 +2,36 @@
 
 import random
 
-sp = [int(random.randint(0, 1000)) for _ in range(100)]
+sp = [int(random.randint(0, 1000)) for _ in range(10)]
 
 
-def task1(sp):
+def task1a(sp):
     '''1 задание'''
+    ind = int(input('Введите индекс элемента: '))
+    elem = int(input('Введите значение элемента: '))
+    sp.insert(ind, elem)
+    return sp
+
+
+print(task1a(sp))
+
+
+def task1b(sp):
+    '''Добавить элем в заданое место списка алгоритмически'''
+    ind = int(input('Введите индекс элемента: '))
+    elem = int(input('Введите значение элемента: '))
+    new_sp = []
+    for i in range(len(sp)):
+        if i == ind:
+            new_sp.append(elem)
+        else:
+            new_sp.append(sp[i])
+
+    return new_sp
+
+
+print(task1b(sp))
+
+
+def task2(sp):
+    pass
