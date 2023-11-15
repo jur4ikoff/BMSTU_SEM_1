c75b6561dd6956966842f708344827f377711082
1234 <1234>
2023-11-16 01:51:08 +0300

    lab 8 def

--- lab 8/8 de3f.py
+++ lab 8/8 de3f.py
@@ -1,26 +1,28 @@
-def threeSum(nums):
-    res = []
+def threeSum(nums, target):
     n = len(nums)
     nums = sorted(nums)
     for i in range(n - 2):
         a = nums[i]
+        delta = 10 ** 10
         start = i + 1
         end = n - 1
-        while (start < end):
-            b = nums[start]
-            c = nums[end]
-            if (a + b + c == 0):
-                if [a, b, c] not in (res):
-                    res.append([a, b, c])
-                start += 1
-                end -= 1
 
-            elif (a + b + c > 0):
-                end -= 1
+        new_target = target - nums[i]
+
+        while start < end:
+            val = nums[start] + nums[end]
+
+            if abs(delta) > abs(val - target):
+                delta = target - val
+
+            if val == new_target:
+                return target
+            elif val < new_target:
+                start += 1
             else:
-                start += 1
+                end -= 1
 
-    return res
+        return target - delta
 
 
-print(threeSum([-1, 0, 1, 2, -1, -4]))
+print(threeSum([-1, 0, 1, 2, -1, -4], 10))
